acd9860450baa980e5cbc435e34b90233db99c07
Diarra <dev.diarra@gmail.com>
2015-03-26 06:57:59 +0100

    debut de dev

--- shop-market-app/Brocfile.js
+++ shop-market-app/Brocfile.js
@@ -4,24 +4,30 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var env = process.env.EMBER_ENV;
 
 var app = new EmberApp({
-	 storeConfigInMeta: false,
-      wrapInEval: false
+  storeConfigInMeta: false
+  //    wrapInEval: false,
+  //
+  //'ember-cli-bootstrap': {
+  ////  'importBootstrapJS': true,
+  ////  'importBootstrapTheme': true,
+  //  'importBootstrapFont': true
+  //}
 
 });
-console.log(env);
+
 if (env === 'development') {
 
-  app.options.inlineContent = {
-    'appJS':{
-    	file:'./app/app.js',
-    	enabled: true
-    },
-
-    'appCSS':{
-    	file:'./app/styles/app.css',
-    	enabled:true
-    }
-  };
+  //app.options.inlineContent = {
+  //  'appJS':{
+  //  	file:'app/app.js',
+  //  	enabled: true
+  //  },
+  //
+  //  'appCSS':{
+  //  	file:'app/styles/app.css',
+  //  	enabled:true
+  //  }
+  //};
 }
 
 // Use `app.import` to add additional libraries to the generated
@@ -36,5 +42,6 @@ if (env === 'development') {
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-app.import('/bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 module.exports = app.toTree();
